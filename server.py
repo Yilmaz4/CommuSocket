34dd8fa061a003d4fc83867ef1fe9dd1d8187540
Yilmaz Alpaslan <ymzymz2007@gmail.com>
2023-07-23 03:43:57 +0300

    begin use of functions instead of if/elif statements

--- server.py
+++ server.py
@@ -11,24 +11,22 @@ class MainServer(object):
         self.users: dict[Address, Optional[Server]] = {}
         self.clients: dict[Address, socket.socket] = {}
 
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        try:
+            self.localIP = s.getsockname()[0]
+        finally:
+            s.close()
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.get_localIP(), PORT))
+        self.sock.bind((self.localIP, PORT))
         self.sock.listen(5)
-        print(f"Server started. Listening on {':'.join((self.get_localIP(), str(PORT)))}...")
+        print(f"Server started. Listening on {':'.join((self.localIP, str(PORT)))}...")
         while True:
             client, address = self.sock.accept()
             address_obj = Address(address[0], address[1])
             self.clients[address_obj] = client
             threading.Thread(target=self.listen_to_client, args=(client, address_obj)).start()
-
-    @staticmethod
-    def get_localIP() -> str:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        try:
-            return s.getsockname()[0]
-        finally:
-            s.close()
 
     def listen_to_client(self, client: socket.socket, address: Address):
         self.users[address] = None
@@ -40,18 +38,20 @@ class MainServer(object):
             except ConnectionResetError:
                 return
             if data:
+                data.split(b'/')
+
                 if data.startswith(b"CREATE_SERVER"):
                     details: dict[str, str | int] = pickle.loads(data.split(b'|')[1])
                     self.servers.append(Server(address, details["name"], details["capacity"], details["password"]))
                     self.users[address] = self.servers[-1]
                     client.send(b"SUCCESS")
-                elif data.startswith(b"GET_SERVERS"):
+                elif data.startswith(b"GET_MASTERLIST"):
                     client.send(pickle.dumps(self.servers))
                 elif data.startswith(b"JOIN_SERVER"):
                     server: Server = pickle.loads(data.split(b'|')[1])
                     server.append_user(address)
                     client.send(b"SUCCESS")
-                elif data.startswith(b"LEAVE_SERVER"):
+                elif data.startswith(b"QUIT_SERVER"):
                     server: Server = pickle.loads(data.split(b'|')[1])
                     server.remove_user(address)
                     client.send(b"SUCCESS")
@@ -61,6 +61,25 @@ class MainServer(object):
                         member.send(b"SEND_MESSAGE|" + message)
             else:
                 continue
+
+    def create_server(self, client, data):
+        details: dict[str, str | int] = pickle.loads(data.split(b'|')[1])
+        self.servers.append(Server(address, details["name"], details["capacity"], details["password"]))
+        self.users[address] = self.servers[-1]
+        client.send(b"SUCCESS")
+
+    def get_masterlist(self, client, data):
+        client.send(pickle.dumps(self.servers))
+        
+    def join_server(self, client, data):
+        ...
+
+    def quit_server(self, client, data):
+        ...
+
+    def send_message(self, client, data):
+        ...
+
 
 if __name__ == "__main__":
     MainServer()
